228c363326f80cfac83c162d5d057600889df145
ohs70230 <taroo1512@gmail.com>
2019-05-31 09:11:09 +0900

    rss取得時リンクが出るように変更

--- js/rss-feed.js
+++ js/rss-feed.js
@@ -1,25 +1,28 @@
-$('#button').click(function() {
-	var i = 1;
-	var val = $("input[name='news_url']:checked").val();
+function send_ajax () {
 	$.ajax({
-		url: 'model/index_rss.php',
-		type: 'GET',
-		dataType: 'xml',
+		url: 'model/index_rss.php',			// データ送り先のphpファイルのパス(相対)
+		type: 'GET',						// 送信データタイプ
+		dataType: 'xml',					// 受信データのファイル形式
 		data: {
-			'news_url': $val,
+			// チェックしているラジオボタンのvalueを、'model/index_rss.php'にgetタイプで送信
+			'url': $("input[name='url']:checked").val(),
 		},
+
+		// 'model/index_rss.php'で画面出力された文字列を、'xml'形式で変数dataに格納。
 		success: function(data) {
+			var i = 1;
+			$('#output').empty();
+			$('#url-error').remove();
 			$(data).find('item').each(function() {
 				titleTxt = $(this).find('title').text();
 				pubdateTxt = $(this).find('pubDate').text();
+				link = $(this).find('link').text();
 				$("#output").append("<div class='box' id='item"+i+"'></div>");
-				$("#output #item"+i).append("<h2>"+titleTxt+"</h2>");
+				$("#output #item"+i).append("<a href='"+link+"'><h2>"+titleTxt+"</h2></a>");
 				$("#output #item"+i).append("<p>"+pubdateTxt+"</p>");
 				$("#output #item"+i).append("<hr>");
 				i++;
 			})	// find
 		},
-		error: function(){
-		}
 	})
-})
+}
--- model/index_rss.php
+++ model/index_rss.php
@@ -1,8 +1,8 @@
 <?php
 	header("Content-type: application/xml; charset=UTF-8");
 	$url = "";
-	if (isset($_GET['news_url'])) {
-		$url = $_GET['news_url'];
+	if (isset($_GET['url'])) {
+		$url = $_GET['url'];
 	}
 	$xml = file_get_contents($url);
 	print $xml;
